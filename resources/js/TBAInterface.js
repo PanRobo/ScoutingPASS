c5e3458c33090901daf4f06165e9fd8ac1b77d65
PanRobo <73448392+PanRobo@users.noreply.github.com>
2023-07-05 18:20:36 -0500

    Update TBAInterface.js

--- resources/js/TBAInterface.js
+++ resources/js/TBAInterface.js
@@ -1,7 +1,7 @@
 // TBAInterface funcitons to pull data from TheBlueAlliance.com
 var teams = null;
 var schedule = null;
-var authKey = "uTHeEfPigDp9huQCpLNkWK7FBQIb01Qrzvt4MAjh9z2WQDkrsvNE77ch6bOPvPb6";
+var authKey = "MxzZdaBUXDzAM0R8vyZ1lO1K0eAUmIfWsaKZjwjeldEeH09XlVKbdJY2BcHJYAw5";
 /**
  * Get list of teams in event
  *
